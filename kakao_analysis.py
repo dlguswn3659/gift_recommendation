b64b9d421162ccca77b125494c7bcdf1c32a4788
Hyunju <hyunjulee@image.cau.ac.kr>
2020-11-22 05:29:44 +0900

    Update kakao_analysis.py

--- kakao_analysis.py
+++ kakao_analysis.py
@@ -6,7 +6,7 @@ from google.colab import files
  
 words = []
 
-with open(r'/content/파일이름.txt', 'r', encoding='utf-8') as f:
+with open(r'/content/파일 이름.txt', 'r', encoding='utf-8') as f:
     for line in f:
         # m = re.search(r"^.*?\[\d\d:\d\d\]\s*(.+)$", line)
         # print(m)
@@ -14,11 +14,19 @@ with open(r'/content/파일이름.txt', 'r', encoding='utf-8') as f:
         if matchOB:
           # print(matchOB.group())
           m = re.sub("^\[(.+\]?)\[\w+.\d\d:\d\d\]","",line)
-          print(m)
+          # print(m)
  
         if m:
             # words.extend(re.split(r"\s+", m.group(1)))
-            words.extend(m.split())
+            wanted_items = re.search('갖고 싶|갖고싶|사고 싶|사고싶|싶다|싶어|싶은|싶고', line)
+            if wanted_items:
+              # words.extend(m.split())
+              wanted_items2 = re.search('갖고 싶|갖고싶|사고 싶|사고싶|싶다|싶어|싶은|싶고', m)
+
+              if wanted_items2:
+                print(wanted_items2.group())
+                print(m)
+              # words.extend(wanted_items.group())
  
-for word, freq in Counter(words).most_common(10):
-    print("{0:10s} : {1:3d}".format(word, freq))
+# for word, freq in Counter(words).most_common(10):
+#     print("{0:10s} : {1:3d}".format(word, freq))
